e17485aa0032f01bea56ac3695c933698cdc9b4d
sumanth <mrsumanth1947@gmail.com>
2024-12-22 09:16:05 +0530

    fix redirect api error but stuck in /me/messages endpoint

--- EmailEngineBE/src/controllers/emailAuthentication.js
+++ EmailEngineBE/src/controllers/emailAuthentication.js
@@ -7,7 +7,7 @@ const router = Router();
 export default router.get("/", async (req, res) => {
   try {
     const authCodeUrlParameters = {
-      scopes: ["Mail.read"],
+      scopes: ['Mail.Read', 'User.Read'],
       redirectUri: "http://localhost:3000/redirect",
     };
 
--- EmailEngineBE/src/helper/auth.js
+++ EmailEngineBE/src/helper/auth.js
@@ -3,12 +3,12 @@ import * as msal from "@azure/msal-node";
 const msalConfig = {
   auth: {
     clientId: process.env.CLIENT_ID,
-    authority: process.env.AAD_ENDPOINT + "/" + process.env.TENANT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
+    authority: process.env.AAD_ENDPOINT + "/" + process.env.TENANT_ID + "/v2.0",
+    // clientSecret: process.env.CLIENT_SECRET,
   },
 };
 
-const pca = new msal.ConfidentialClientApplication(msalConfig);
+const pca = new msal.PublicClientApplication(msalConfig);
 
 export default pca;
 
